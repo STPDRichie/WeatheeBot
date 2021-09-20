169a11bf8def2a3b6a077aca42f49c9677cede9f
Коляска _ <nikolaj.karpov.2002@mail.ru>
2021-09-20 13:32:44 +0500

    Replacing the project with Spring Initializr
    Чистый проект IDEA удалён, вместо него поставлен шаблон Spring

---
+++ Weather_Bot/src/test/java/com/example/Weather_Bot/WeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Weather_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
