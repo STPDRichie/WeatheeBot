5e50c8c47a6b677bc6ef2936b5ae14dbcc65e6d8
Коляска _ <nikolaj.karpov.2002@mail.ru>
2021-09-22 23:13:53 +0500

    First Working Build
    Echo-Bot

--- STPDWeather_Bot/src/main/java/Bot.java
+++ STPDWeather_Bot/src/main/java/Bot.java
@@ -1,4 +1,3 @@
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
--- STPDWeather_Bot/src/main/java/Main.java
+++ STPDWeather_Bot/src/main/java/Main.java
@@ -1,6 +1,4 @@
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
